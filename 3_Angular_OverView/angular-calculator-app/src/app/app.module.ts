dcd7d7fe47459c86c3d3fd45516fe61c121e5620
thanhbinhkd95 <81392431+thanhbinhkd95@users.noreply.github.com>
2021-12-02 23:31:08 +0700

    bai tap 2 nam trong bai tap 1 Linh oi

--- 3_Angular_OverView/angular-calculator-app/src/app/app.module.ts
+++ 3_Angular_OverView/angular-calculator-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FormsModule } from '@angular/forms';
+import { ColorComponent } from './color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
---
+++ 3_Angular_OverView/angular-calculator-app/src/app/color/color.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-color",
+  templateUrl: "./color.component.html",
+  styleUrls: ["./color.component.css"],
+})
+export class ColorComponent implements OnInit {
+  color1?: string;
+  color2?: string;
+  color3?: string;
+  arr = [
+    { id: 1, vn: "kiemtra", en: "test" },
+    { id: 2, vn: "hoat dong", en: "activity" },
+  ];
+  pickColor(value1, value2, value3) {
+    this.color1 = value1;
+    this.color2 = value2;
+    this.color3 = value3;
+  }
+
+  pickArr(id: number) {
+    const index = this.arr.findIndex((word) => word.id === id);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
